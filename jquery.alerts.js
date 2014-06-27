48cdb8431fe152e5494901168dfc941eb1836d0a
William Gallios <williamgallios@gmail.com>
2014-06-27 13:24:14 -0400

    updates to alerts lib

--- jquery.alerts.js
+++ jquery.alerts.js
@@ -4,19 +4,37 @@
 	{
 		debug: false,
 		type: 'warning', // warning, success, danger, info
-		width: '300px',
-		height: 'auto',
-		x: 15,
-		y: 15,
-		opacity:0.95,
+		opacity:0.9,
 		borderRadius:10,
 		clearTimeoutSeconds:3, // 0 for no timeout
+		animation:
+		{
+			duration:400	
+		},
+		container:
+		{
+			id: 'jquery-alerts-container',
+			clas: 'jquery-alerts-container', // default container class
+			width: '50%',
+			x: 30,
+			y: 30
+		},
 		defaultHeaders:
 		{
 			warning: 'Warning',
 			success: 'Success',
-			danger: 'Danger',
+			danger: 'Error',
 			info: 'Information'
+		},
+		icon: // default icons for vaiours icon packages
+		{
+			fontawesome:
+			{
+				warning: 'fa fa-exclamation-triangle',
+				success: 'fa fa-thumbs-up',
+				danger: 'fa fa-times-circle-o',
+				info: 'fa fa-info-circle',	
+			}
 		}
 	};
  	
@@ -37,43 +55,88 @@
 	
 	fn.init = function ()
 	{
-
+		this.checkFontAwesome();
+		this.buildContainer();
+	}
+	
+	fn.buildContainer = function ()
+	{
+		var o = this.options;
+		
+		if ($('#' + o.container.id).length <= 0)
+		{
+			$container = $("<div>", {id: o.container.id, class: o.container.clas});
+			
+			$container.css('width', o.container.width)
+				.css('top', o.container.x)
+				.css('left', o.container. y);
+			
+			this.$container = $container;
+		
+			this.$el.prepend($container);
+		}
+		
+		return true;
 	}
 
 	fn.buildAlert = function (type, msg, header)
 	{
+		var t = this;
 	
 		var o = this.options;
 	
 		if (type == undefined) type = o.type;
+
+		type = type.toLowerCase(); // ensures type is lower case
+		
+		if (header == undefined)
+		{		
+			header = o.defaultHeaders[type];
+		}
 		
 		var $alert = $("<div>", { class:'jquery-alert jquery-alert-' + type });
-			
-		console.log(this.options);
-		console.log(o);
-						
-		$alert.css('opacity', 0)
-			.css('width', o.width)
-			.css('height', o.height)
-			.css('left', o.x)
-			.css('top', o.y)
-			.css('border-radius', o.borderRadius);
 		
-		var html = "<h3>" + o.header + "</h3>" + 
+		var $closeBtn = $("<button>", { type:'button', class:'close-alert' });
+		$closeBtn.html('&times;');
+		
+		$alert.prepend($closeBtn);
+		
+		$closeBtn.click(function(e){
+			t.clearAlert($alert);
+		});
+		
+		
+		if (this.fontawesome)
+		{
+			header = "<i class='" + o.icon.fontawesome[type] + "'></i> " + header;
+		}
+		
+		var html = "<h3>" + header + "</h3>" +
 		"<span class='alert-text'>" + msg + "</span>";
 		
 		
 		$alert.append(html);
 		
-		this.$el.prepend($alert);
+		this.$container.prepend($alert);
+				
+		var h = $alert.outerHeight(); // gets final height for alert
+		
+		$alert.css('opacity', 0)
+			.css('height', 0 + 'px')
+			.css('border-radius', o.borderRadius);
+		
+
 		
 		if (this.velocity)
 		{
-			$alert.velocity({ opacity:o.opacity });
+			$alert.velocity({ opacity:o.opacity, height:h });
+			//$alert.velocity('slideDown', {});
+
 		}
 		else
 		{
-			$alert.css('opacity', o.opacity);
+			$alert.css('opacity', o.opacity)
+				.css('height', '');
 		}
 		
 		return $alert;
@@ -89,24 +152,102 @@
 		
 		var $alert = this.buildAlert('warning', msg, header);
 		
+		t.setAlertTimeout($alert);
+	}
+	
+	fn.success = function (msg, header)
+	{
+		var t = this;
+		
+		if (msg == undefined || msg == '') return false;
+		
+		if (header == undefined) header = this.options.defaultHeaders.success;
+		
+		var $alert = this.buildAlert('success', msg, header);
+		
+		t.setAlertTimeout($alert);
+	}
+
+	fn.danger = function (msg, header)
+	{
+		var t = this;
+		
+		if (msg == undefined || msg == '') return false;
+		
+		if (header == undefined) header = this.options.defaultHeaders.danger;
+		
+		var $alert = this.buildAlert('danger', msg, header);
+		
+		t.setAlertTimeout($alert);
+	}
+	
+	fn.info = function (msg, header)
+	{
+		var t = this;
+		
+		if (msg == undefined || msg == '') return false;
+		
+		if (header == undefined) header = this.options.defaultHeaders.info;
+		
+		var $alert = this.buildAlert('info', msg, header);
+		
+		t.setAlertTimeout($alert);
+	}
+	
+	fn.setAlertTimeout = function ($alert)
+	{
+		var t = this;
+				
 		if (t.options.clearTimeoutSeconds > 0)
 		{
 			setTimeout(function(){
 				t.clearAlert($alert);
 			}, (t.options.clearTimeoutSeconds * 1000));	
 		}
+	}
+	
+	fn.checkFontAwesome = function ()
+	{
+		var fa = false;
 		
+		$('head link').each(function(i, f){
+			var href = $(f).attr('href');
+			
+			if (href == undefined) return false;
+			if (href == '') return false;
+			
+			if (href.indexOf('font-awesome') > 0)
+			{
+				fa = true;
+				return true;
+			}
+			
+		});
+		
+		this.fontawesome = fa;
+		
+		return fa;
 	}
 
 	fn.clearAlert = function ($alert)
 	{
+		var duration = this.options.animation.duration;
+		
 		if (this.velocity)
 		{
-			$alert.velocity({ opacity:0 });
+			$alert.velocity({ opacity:0 }, {
+				duration: duration,
+				complete:function()
+				{
+					$(this).remove();
+				}
+			});
 		}
 		else
 		{
-			$alert.css('opacity', 0);
+			$alert.fadeOut(duration, function(){
+				$alert.remove();
+			})
 		}
 		
 	}
